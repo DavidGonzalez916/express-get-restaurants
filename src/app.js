754921b6a943acfad8370d4bbd65bf706d6d26ca
DavidGonzalez916 <dagonzalez@deloitte.com>
2024-03-27 13:17:52 -0400

    initial commit

--- src/app.js
+++ src/app.js
@@ -4,11 +4,15 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-app.get("/restaurants", async (req, res) => {
-    const restaurants = await Restaurant.findAll({});
+app.get("/restaurants/:id", async (req, res) => {
+    const restaurants = await Restaurant.findByPk(req.params.id);
     res.json(restaurants);
 })
 
+app.use(express.json());
+app.post("/restaurants", (req, res) => {
+    
+})
 
 
 module.exports = app;
